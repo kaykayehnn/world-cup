9ccdadc8e7da7042745570f1703f110162976a07
Krasimir Nedelchev <kayKayEhnn@outlook.com>
2018-07-11 22:42:39 +0300

    Add teamMatches endpoint, refactor football modules
    Possible the best thing I've written. Also reduce cache time to 30s.

--- server/controllers/team-controller.js
+++ server/controllers/team-controller.js
@@ -1,9 +1,10 @@
 const Team = require('../models/Team')
-const { getTeamStatistics } = require('../apis/football')
+const { getTeamStatistics, getTeamMatches } = require('../apis/football')
 
 exports.getTeams = (req, res) => {
   let pr1 = Team.find({}, { _id: 0, name: 1 }).lean()
   let pr2 = getTeamStatistics(req.cache)
+
   Promise.all([pr1, pr2])
     .then(([teams, stats]) => {
       let result = teams.map(t => ({ name: t.name }))
@@ -13,5 +14,16 @@ exports.getTeams = (req, res) => {
       }
 
       res.json(result)
+    })
+}
+
+exports.getTeamMatches = (req, res, next) => {
+  getTeamMatches(req.params.teamName)(req.cache)
+    .then(data => {
+      if (data.length === 0) {
+        return void next(new Error(`Team ${req.params.teamName} doesn't exist`))
+      }
+
+      res.json(data)
     })
 }
