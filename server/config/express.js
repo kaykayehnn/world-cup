7c83f357d42577c433bdb5314921cce43a052f4d
Krasimir Nedelchev <kayKayEhnn@outlook.com>
2018-07-11 18:43:49 +0300

    Add logger middleware

--- server/config/express.js
+++ server/config/express.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const decodeUser = require('../middleware/decodeUser')
 const attachCache = require('../middleware/attachCache')
@@ -10,6 +11,7 @@ module.exports = (app, cache) => {
   app.use(cookieParser())
   app.use(express.json())
   app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'))
   app.use(express.static(path.resolve(__dirname, '../../public')))
 
   app.use(decodeUser(cache))
