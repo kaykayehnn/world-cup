ae734c72bf65960394c917cb93e2f1f100294776
Krasimir Nedelchev <kayKayEhnn@outlook.com>
2018-07-12 11:24:35 +0300

    Add teamMatches endpoint, refactor football modules

---
+++ server/apis/filterMatches.js
@@ -0,0 +1,14 @@
+module.exports = function filterMatches (teamName) {
+  return function (data) {
+    let matches = []
+    let upperName = teamName.toUpperCase()
+    for (let match of data.matches) {
+      if (match.homeTeam.name.toUpperCase() === upperName ||
+        match.awayTeam.name.toUpperCase() === upperName) {
+        matches.push(match)
+      }
+    }
+
+    return matches
+  }
+}
