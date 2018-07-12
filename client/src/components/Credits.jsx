75d0db5a547f376f38c9240198d8cfbe59689e09
Krasimir Nedelchev <kayKayEhnn@outlook.com>
2018-07-12 21:11:33 +0300

    Update credits

--- client/src/components/Credits.jsx
+++ client/src/components/Credits.jsx
@@ -20,6 +20,14 @@ const credits = [
   {
     label: 'Arrow',
     link: '<div>Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>'
+  },
+  {
+    label: 'Gold/Silver medals',
+    link: '<div>Icons made by <a href="https://www.flaticon.com/authors/twitter" title="Twitter">Twitter</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>'
+  },
+  {
+    label: 'Close X',
+    link: '<div>Icons made by <a href="https://www.flaticon.com/authors/chanut" title="Chanut">Chanut</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>'
   }
 ]
 
