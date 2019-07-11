b269d51d6ada053092e8df8a931695d78a2f2627
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-07-11 17:35:06 +0300

    Fix 404.html
    Webpack requires publicPath not be empty or 404.html requests
    bundle.js and other assets relative to url instead of relative to
    publicPath.

--- client/src/components/Common/Image.jsx
+++ client/src/components/Common/Image.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const PUBLIC_PATH = process.env.PUBLIC_PATH;
+// Remove leading forward slash in case there is one as it breaks image urls.
+const PUBLIC_PATH = process.env.PUBLIC_PATH.replace(/\/$/, "");
 
 const Image = ({ src, ...props }) => <img src={PUBLIC_PATH + src} {...props} />;
 export default Image;
