f911f83410a35bb46fb15f88e3e202b1a0fe4bb4
Krasimir Nedelchev <kaykayehnn@gmail.com>
2020-03-09 23:33:03 +0200

    Add github corner link

--- client/src/containers/Root.jsx
+++ client/src/containers/Root.jsx
@@ -5,11 +5,13 @@ import { hot } from "react-hot-loader";
 
 import Navbar from "./Public/NavbarContainer";
 import Router from "./Router";
+import GithubCorner from "react-github-corner";
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <div className="content">
       <div className="container">
+        <GithubCorner href="https://github.com/kaykayehnn/russia-wc" />
         <Router>
           <Navbar />
         </Router>
